b43a0bf0606e0bfab15076ac01c2dfff0ec8e874
Greq <greqorian@gmail.com>
2021-04-10 15:24:24 +0200

    build: :sparkles: Project initialisation

---
+++ javascript.js
@@ -0,0 +1,37 @@
+const notes = [
+
+    {
+        text: "go learn",
+        completed: true
+    },
+
+    {
+        text: "shopping",
+        completed: false
+    },
+
+    {
+        text: "sleep",
+        completed: true
+    },
+
+]
+
+
+
+
+const completed = notes.filter(function(note,index){
+    return note.completed === true;
+}).length
+
+const  newP = document.createElement("p");
+   newP.textContent = `you have ${completed} completed tasks`;
+   document.querySelector("body").appendChild(newP);
+
+notes.forEach(function (todo, index) {
+
+   const  newP = document.createElement("p");
+   newP.textContent = todo.text;
+   document.querySelector("body").appendChild(newP);
+
+})
